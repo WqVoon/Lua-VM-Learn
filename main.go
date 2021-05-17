36b105af377c2658fc91be640ef6c2f5743a35a8
WqVoon <2027195958@qq.com>
2021-05-17 14:02:10 +0800

    在 main.go 中对 LuaState 栈操作相关的函数进行了测试

--- main.go
+++ main.go
@@ -1,23 +1,56 @@
 package main
 
 import (
-	"io/ioutil"
-	"lua-vm/binchunk"
-	"os"
+	"fmt"
+	. "lua-vm/api"
+	"lua-vm/state"
 )
 
 func main() {
-	var filename string
-	if len(os.Args) != 1 {
-		filename = os.Args[1]
-	} else {
-		filename = "luac.out"
-	}
+	ls := state.New()
 
-	data, err := ioutil.ReadFile(filename)
-	if err != nil {
-		panic(err)
-	}
+	ls.PushBoolean(true)
+	printStack(ls)
 
-	binchunk.List(binchunk.Undump(data))
+	ls.PushInteger(10)
+	printStack(ls)
+
+	ls.PushNil()
+	printStack(ls)
+
+	ls.PushString("hello")
+	printStack(ls)
+
+	ls.PushValue(-4)
+	printStack(ls)
+
+	ls.Replace(3)
+	printStack(ls)
+
+	ls.SetTop(6)
+	printStack(ls)
+
+	ls.Remove(-3)
+	printStack(ls)
+
+	ls.SetTop(-5)
+	printStack(ls)
+}
+
+func printStack(ls LuaState) {
+	top := ls.GetTop()
+	for i := 1; i <= top; i++ {
+		t := ls.Type(i)
+		switch t {
+		case LUA_TBOOLEAN:
+			fmt.Printf("[%t]", ls.ToBoolean(i))
+		case LUA_TNUMBER:
+			fmt.Printf("[%g]", ls.ToNumber(i))
+		case LUA_TSTRING:
+			fmt.Printf("[%q]", ls.ToString(i))
+		default:
+			fmt.Printf("[%s]", ls.TypeName(t))
+		}
+	}
+	fmt.Println()
 }
